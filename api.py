1d2655cc349dbd2f2f0a887703766b1087214f1e
jules <juju.fayol@gmail.com>
2024-12-07 23:42:04 +0000

    feat: premier programme

--- api.py
+++ api.py
@@ -1,4 +1,12 @@
-import ray
-import fastapi
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue sur FastAPI!"}
+
+@app.get("/second/")
+def read_root():
+    return {"message": "2eme page"}
 
